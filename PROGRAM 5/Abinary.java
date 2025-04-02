ad3b98e46a15b12c1c2513a8b5ca16fe2c8f44ca
asswiin <aswinhareesh1991@gmail.com>
2025-04-02 15:35:43 +0530

    Add files via upload

---
+++ PROGRAM 5/Abinary.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class binary {
+    void define(int arr[], int n, int item) {
+        Arrays.sort(arr);
+        int result = Arrays.binarySearch(arr, item);
+        if (result >= 0) {
+            System.out.println("\nItem found at position: " + (result + 1));
+        } else {
+            System.out.println("\nItem cannot be found");
+        }
+    }
+
+    public static void main(String args[]) {
+        int n, item, i;
+        Scanner sc = new Scanner(System.in);
+        Binaryarray bin = new Binaryarray();
+        System.out.print("\nEnter the number of elements : ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("\nEnter " + n + " numbers : ");
+        for (i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+       System.out.println("\nEnter the number to be searched : ");
+        item = sc.nextInt();
+        bin.define(arr, n, item);
+    }
+}
