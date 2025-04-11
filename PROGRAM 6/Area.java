3d18e3a5555cb9a770c0de559b354bc3f3e3a72c
asswiin <aswinhareesh1991@gmail.com>
2025-04-11 14:28:06 +0530

    Add files via upload

---
+++ PROGRAM 6/Area.java
@@ -0,0 +1,66 @@
+//WRITE A PROGRAM TO FIND AREA OF DIFFERENT SHAPES USING OVERLOADED FUNCTION 
+
+import java.util.Scanner;     
+
+class Area
+{
+    
+    double area(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+    
+    double area(double length, double width)
+    {
+        return length * width;
+    }
+    
+    double areaTriangle(double base, double height) 
+    {
+        return 0.5 * base * height;
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Area areaCalculator = new Area();
+        
+        System.out.println("\nChoose the shape to calculate area:");
+        System.out.println("1. Circle");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Triangle");
+        System.out.print("Enter an option : ");
+        
+        int choice = scanner.nextInt();
+        
+        switch (choice) {
+            case 1:
+                System.out.print("\nEnter the radius of the circle: ");
+                double radius = scanner.nextDouble();
+                System.out.println("Area of Circle: " + areaCalculator.area(radius));
+                break;
+                
+            case 2:
+                System.out.print("\nEnter the length of the rectangle: ");
+                double length = scanner.nextDouble();
+                System.out.print("Enter the width of the rectangle: ");
+                double width = scanner.nextDouble();
+                System.out.println("Area of Rectangle: " + areaCalculator.area(length, width));
+                break;
+                
+            case 3:
+                System.out.print("\nEnter the base of the triangle: ");
+                double base = scanner.nextDouble(); 
+                System.out.print("Enter the height of the triangle: ");
+                double height = scanner.nextDouble();
+                
+                System.out.println("Area of Triangle: " + areaCalculator.areaTriangle(base, height)); 
+                break;
+                
+            default:
+                System.out.println("\nInvalid choice.");
+        }
+        
+        scanner.close();
+    }
+}
